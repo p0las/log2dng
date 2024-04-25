f0d27ebe316a43d01a0f0c3ca8303464a116418c
Pawel Olas <olo@polas.net>
2024-04-25 15:52:47 +1000

    removed tests that weren't tests

--- src/log2dngTest.py
+++ src/log2dngTest.py
@@ -3,16 +3,10 @@ import pytest
 from log2dng import dngConvert
 from matrices import CATs
 
+@pytest.mark.skip("not a test but a conversion for manual inspection")
 def test_performConversion():
     dngConvert(r"F:\stills\log2dng_raw_compare2\ACEScg_daylight.tif", r"F:\stills\log2dng_raw_compare2\ACEScg_daylight.dng", "ACES - ACEScc")
-    # dngConvert(r"F:\stills\log2dng_raw_compare2\ACEScg_shade.tif", r"F:\stills\log2dng_raw_compare2\ACEScg_shade.dng", "ACES - ACEScc")
 
-@pytest.mark.parametrize("cat", CATs)
-def test_cats(mocker, cat):
-
-    mocker.patch('dng.DNG_MATRIX', cat)
-
-    dngConvert(r"F:\stills\log2dng_raw_compare2\ACEScg_daylight.tif", r"F:\stills\log2dng_raw_compare2\cat{0}.dng".format(cat[0][0]), "ACES - ACEScc")
 
 
 
